1b42d263d7529d7799955cdd6813029224b3ae28
Zaah91 <zaah91@gmail.com>
2021-09-28 18:41:41 +0200

    exersice_3 done

--- src/com/company/Exersice_3.java
+++ src/com/company/Exersice_3.java
@@ -4,11 +4,14 @@ public class Exersice_3 {
 
     public static void main(String[] args) {
         MyDate MyDate1 = new MyDate(12,"january",2211);
-
+        // Værdierne indsættes i day/month/year som står som private i filen MyDate
         System.out.println(MyDate1);
 
         MyDate Birthday = new MyDate(8,"august",1991);
-
+        // Værdierne indsættes i day/month/year som står som private i filen MyDate
+        // Denne gang i sammenhæng med at lave det om til ens fødselsdag.
         System.out.println(Birthday);
+
+        // Som man nu kan se, så skriver den begge prints ud, som forskellige.
     }
 }
--- src/com/company/MyDate.java
+++ src/com/company/MyDate.java
@@ -4,15 +4,24 @@ public class MyDate {
     private int day;
     private String month;
     private int year;
+        // private er kun muligt at bruge lokalt i koden
+        // public kan tilgås udenfor
+        // De er ikke definerede grundet at den får information fra Exersice_3.
+        // De data/information der kommer fra Exersice_3 bliver der efter kørt igennem systemet og
+        // udskrevet ved hjælp at toString der kommer nederst i denne fil.
 
     MyDate(int day, String month, int year){
         this.day = day;
         this.month = month;
         this.year = year;
+        // integers grundet at vi skal bruge talværdier uden komma
+        // String grundet at vi skal bruge flere bogstaver end et (shar)
+        // this. kan udskiftes, men forstod ikke hvordan
+        // Dette kan ses i den kode der er lavet af Jens Peter
     }
 
     public String toString(){
         return "Day: " + day + " Month: " + month + " Year: " + year;
+        // toString bruges for at gøre det print der kommer læsbart.
     }
-
 }
